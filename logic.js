6bf81f49119dae7245eca15709f9c82b4407b8bd
Rodrigo <rodrigocralha@gmail.com>
2022-06-30 21:25:32 +0100

    logica já está melhor, falta configurar os comandos do ogo

--- logic.js
+++ logic.js
@@ -1,38 +1,61 @@
+let cubes;
+
 function changeCubeState(e)
 {
-    console.log(e)
+    cubes = document.querySelectorAll(".cube");
+
+    if(e == undefined) return;
 
     if( this.classList.contains("clicked") )
     {
+        this.style.backgroundColor = "red";
         this.classList.remove("clicked");
         return;
     }
     
+    this.style.backgroundColor = "blue";
     this.classList.add("clicked");
 }
 
 function generateGrid(table, userOption)
 {
-    for(let i = 0; i < userOption ** 2; i++)
+    table.innerHTML = '';
+
+    table.style.gridTemplateColumns = `repeat(${userOption}, 1fr)`;
+
+    for(let i = 0; i < userOption**2; i++)
     {
         const newDiv = document.createElement('div');
         newDiv.classList.add("cube");
-        const style = newDiv.getAttribute("style");
-        console.log(style);
-        style.add(`height: ${1000 / userOption}; width: ${1000 / userOption};`);
+
+        //newDiv.style.width = 100 / userOption;
+        //newDiv.style.height = 100 / userOption;
+        newDiv.style.rowGap = 1;
+        //newDiv.style.backgroundColor = "blue";
+
         table.append(newDiv);
     }
 }
 
-const table = document.querySelector(".table");
-const cubes = document.querySelectorAll(".cube");
-const userOption = document.getElementById("gridSize").value;
-
-generateGrid(table, userOption);
-
-if(cubes != undefined)
+function main()
 {
-    cubes.forEach(cube => {
-        cube.addEventListener("mouseover", changeCubeState)
+    /* tabela inicial */
+    generateGrid(table, userOption.value);
+    cubes = document.querySelectorAll(".cube");
+
+    cubes.forEach( (cube) => {
+        cube.addEventListener("mouseenter", changeCubeState);
     });
 }
+
+const table = document.querySelector(".table");
+const userOption = document.querySelector(".config > .gridSize");
+
+main();
+
+userOption.addEventListener("change", () => {
+    main();
+});
+
+
+
